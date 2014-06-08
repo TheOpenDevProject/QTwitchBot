2b993336d4716791a89a88ed5a331655249ef348
TheOpenDevProject <tkgs28@gmail.com>
2014-06-08 16:33:53 +1000

    First UI Design final

--- QTwitchBot.pro
+++ QTwitchBot.pro
@@ -4,7 +4,7 @@
 #
 #-------------------------------------------------
 
-QT       += core gui
+QT       += core gui webkitwidgets
 
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
@@ -21,3 +21,6 @@ HEADERS  += mainwindow.h \
 
 FORMS    += mainwindow.ui \
     monitor.ui
+
+RESOURCES += \
+    localRes.qrc
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <QWebSettings>
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->streamView->settings()->setAttribute(QWebSettings::PluginsEnabled,true);
 }
 
 MainWindow::~MainWindow()
