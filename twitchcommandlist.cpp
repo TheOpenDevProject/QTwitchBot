31b4e73583bd1b5a5fbf8a74a5fe73ff88432fb7
TheOpenDevProject <tkgs28@gmail.com>
2014-06-27 18:32:58 +1000

    Engine Update
    Added ability to flag commands

--- twitchcommandlist.cpp
+++ twitchcommandlist.cpp
@@ -9,6 +9,7 @@ QMap<QString, QString> TwitchCommandList::loadFromFile(QString file_name)
     QFile c_File;
    //Reset the commandTree from any other times the user loaded the file in this session
     commandTree.clear();
+    modCommandTree.clear();
     /////////////////////
     c_File.setFileName(file_name);
     if(!c_File.open(QIODevice::ReadOnly)){
@@ -18,11 +19,26 @@ QMap<QString, QString> TwitchCommandList::loadFromFile(QString file_name)
     while(!c_File.atEnd()){
         //This is quite a simple algorithm,
         // All we are doing is reading in the file line by line and spliting it by csv and adding it to our map
+
    QString keyPair = c_File.readLine();
+   //If we find a Moderator
+   if(keyPair.at(1) == QString("M")){
+       QStringList keyPairListm = keyPair.split(",");
+       keyPairListm.first().remove(0,3);
+    modCommandTree.insert(keyPairListm.at(0),keyPairListm.at(1));
+  qDebug() << "Moderator Command Found";
+  qDebug() << modCommandTree;
+   }else{
    QStringList keyPairList = keyPair.split(",");
         commandTree.insert(keyPairList.at(0),keyPairList.at(1));
         }
+      }
     }
-    qDebug() << commandTree; //#Remove before release
+
     return commandTree;
+}
+
+QMap<QString, QString> TwitchCommandList::getModeratorCommandList()
+{
+    return modCommandTree;
 }
--- twitchcommandlist.h
+++ twitchcommandlist.h
@@ -8,8 +8,10 @@ class TwitchCommandList
 public:
     TwitchCommandList();
     QMap<QString,QString> loadFromFile(QString file_name);
+    QMap<QString,QString> getModeratorCommandList();
 private:
     QMap<QString,QString> commandTree; //Our Key is a string and our return is a string
+    QMap<QString,QString> modCommandTree; //Commands only moderators can run [M]
 };
 
 #endif // TWITCHCOMMANDLIST_H
