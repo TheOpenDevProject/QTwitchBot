53b111b3a0f50fc3db4803947ce82275b1079d4a
TheOpenDevProject <tkgs28@gmail.com>
2014-08-10 02:23:22 +1000

    JSON Parser Template Working

--- apimanager.cpp
+++ apimanager.cpp
@@ -17,6 +17,7 @@ void RiotAPI::requestBasicProfile(QString summoner_name){
     if(riotAPI_Key.isEmpty() || riotAPI_Key.size() < 36){
         qDebug() << "Riot API Key Is Not Valid";
     }else{
+        summonerName = summoner_name;
         qnam->get(QNetworkRequest(QUrl("https://oce.api.pvp.net/api/lol/oce/v1.4/summoner/by-name/"+ summoner_name +"?api_key=" + riotAPI_Key)));
     }
 
@@ -38,21 +39,12 @@ void RiotAPI::ParseAndSet(){
     //We have data, Proceed with JSON specific validation + loading document
         QJsonParseError jerror;
         QJsonDocument jdoc  = QJsonDocument::fromJson(rawapidata,&jerror);
-        //Check if there was an issue parsing the JSON document, If there was just end the entire function execution.
-
-      //  if(jerror.error() != QJsonParseError::NoError){
-  //          qDebug() << "Unable to parse JSON document(" << jerror.errorString() << ")";
-   //     return;
-   //     }
 
        //Get the root element of the basic summoner profile, Currently we can expect this to be the summoners name.
-        QJsonObject jrootObject = jdoc.object();
-        summonerID = jrootObject.keys().at(0);
-        qDebug() << summonerID;
-        QJsonArray jProfileArray = jrootObject["sumcoolaid"].toArray();
-        qDebug() << jProfileArray.at(0);
-
-
+    const QJsonObject jObject = jdoc.object();
+    QJsonValue jVal = jObject.value("sumcoolaid");
+qDebug() << jVal;
+    QJsonObject playerDetails = jVal.toObject();
+    qDebug() << playerDetails.value(playerDetails.keys().at(0)).toString();
 
 }
-
